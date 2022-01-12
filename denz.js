2e685c1387f326190109d863918c11c52bece25b
Pangeran <96149505+Pangerann@users.noreply.github.com>
2022-01-13 02:13:57 +0700

    Update denz.js

--- denz.js
+++ denz.js
@@ -283,7 +283,7 @@ try {
 			}
 		}
 		const botNumber = denz.user.jid
-		const ownerNumber = ["6287832147584@s.whatsapp.net","6285719579904@s.whatsapp.net",`${NomorOwner}@s.whatsapp.net`]
+		const ownerNumber = ["6287832147584@s.whatsapp.net","6285719579904@s.whatsapp.net",`${NomorOwner}@s.whatsapp.net`,`${NomorOwner2}@s.whatsapp.net`]
         const isGroup = from.endsWith('@g.us')
 		const sender = mek.key.fromMe ? denz.user.jid : isGroup ? mek.participant : mek.key.remoteJid
 		const senderNumber = sender.split("@")[0] 
@@ -982,8 +982,9 @@ reply('http://youtube.com/dcodedenpa')
         	case 'menu':
         case 'help':
         dtod = "6287832147584@s.whatsapp.net"
-        dtod = "6285719579904@s.whatsapp.net"
+        dtod2 = "6285719579904@s.whatsapp.net"
      otod = `${settings.NomorOwner}@s.whatsapp.net`
+     otod2 = `${settings.NomorOwner2}@s.whatsapp.net`
         stod = `${sender}`
        stst = await denz.getStatus(`${sender.split('@')[0]}@c.us`)
 				stst = stst.status == 401 ? 'unknown' : stst.status
